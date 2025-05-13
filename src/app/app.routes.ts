5d8974922fb2539fc0e60f0c05df6638a19256c8
MojasSimunovic <mojas0402@gmail.com>
2025-05-13 11:12:31 +0200

    initial project setup and imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GoalListComponent } from './pages/goal-list/goal-list.component';
+import { TaskListComponent } from './pages/task-list/task-list.component';
+import { RemindersComponent } from './pages/reminders/reminders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomepageComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'goals',
+    component: GoalListComponent
+  },
+  {
+    path: 'tasks',
+    component: TaskListComponent
+  },
+  {
+    path: 'reminders',
+    component: RemindersComponent
+  },
+];
