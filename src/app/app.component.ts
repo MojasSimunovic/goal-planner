81f308e1c44f4f7eb66204b2839c392a993fb4b7
MojasSimunovic <mojas0402@gmail.com>
2025-05-13 11:52:38 +0200

    setup of the login/signup modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements AfterViewInit {
   title = 'goal-planner';
+
+  isLoginFormVisible = signal<boolean>(false);
+
+
+  ngAfterViewInit(): void {
+    console.log('started')
+    const modalEl = document.getElementById('loginModal');
+    if (modalEl) {
+      const modal = new bootstrap.Modal(modalEl);
+      // modal.show(); // auto-show on page load
+    }
+  }
 }
