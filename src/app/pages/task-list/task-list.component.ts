fa54d84a9d6c75d8ddb4b4c309665040b6e59158
MojasSimunovic <mojas0402@gmail.com>
2025-05-19 15:20:40 +0200

    finished tasks component

--- src/app/pages/task-list/task-list.component.ts
+++ src/app/pages/task-list/task-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, DestroyRef, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, DestroyRef, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { Task } from '../../model/task';
 import { GoalService } from '../../services/goal.service';
 import { FormsModule } from '@angular/forms';
 import { TaskService } from '../../services/task.service';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-task-list',
-  imports: [FormsModule],
+  imports: [FormsModule, DatePipe, AsyncPipe],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
@@ -16,6 +18,8 @@ export class TaskListComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   goalService = inject(GoalService);
   taskService = inject(TaskService);
+
+  taskList$ : Observable<Task[]> = new Observable<Task[]>();
   newTask: Task = {
   taskId: 0,
   taskName: '',
@@ -34,6 +38,7 @@ export class TaskListComponent implements OnInit {
     if (this.modalRef) {
       const modal = new bootstrap.Modal(this.modalRef);
     }
+    this.getAllTasksByUser();
   }
 
   closeModal() {
@@ -41,6 +46,10 @@ export class TaskListComponent implements OnInit {
     modal?.hide();
   }
 
+  getAllTasksByUser() {
+    this.taskList$ = this.taskService.getTasksByUser();
+  }
+ 
   onSaveTask() {
     const subscription = this.taskService.createTask(this.newTask).subscribe({
       error: (error: Error) => {
@@ -50,6 +59,18 @@ export class TaskListComponent implements OnInit {
     this.destroyRef.onDestroy(()=> {
       subscription.unsubscribe();
     })
+    this.getAllTasksByUser();
     this.closeModal();
+    this.newTask = {
+      taskId: 0,
+      taskName: '',
+      description: '',
+      frequency: '',
+      createdDate: new Date(), // ISO date string
+      startDate: '',   // ISO date string
+      dueDate: '',     // ISO date string
+      isCompleted: false,
+      userId: this.goalService.user.userId
+    }
   }
 }
