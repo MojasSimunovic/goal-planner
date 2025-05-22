556cbf7b0f2454618347e96a6892b9d416c8a0ed
MojasSimunovic <mojas0402@gmail.com>
2025-05-22 13:03:05 +0200

    polished the goal service and delete service

--- src/app/services/goal.service.ts
+++ src/app/services/goal.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Goal } from '../model/goal';
 import { LoggedUserData } from '../model/user';
-import { addDoc, collection, collectionData, doc, Firestore, getDoc, query, setDoc, where } from '@angular/fire/firestore';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, getDoc, query, setDoc, where } from '@angular/fire/firestore';
 import { getAuth } from '@angular/fire/auth';
 import { from, Observable } from 'rxjs';
 
@@ -10,15 +10,9 @@ import { from, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GoalService {
-
-  http = inject(HttpClient);
-
   private userId: string;
-
   firestore = inject(Firestore);
-
   user! : LoggedUserData;
-
   constructor() {
     const auth = getAuth();
     this.userId = auth.currentUser?.uid || '';
@@ -33,19 +27,16 @@ export class GoalService {
 
     return from(setDoc(newDocRef, goalWithId));
   }
-
   getAllGoalsFromUser() {
     if (!this.userId) throw new Error('No user logged in');
     const goalsRef = collection(this.firestore, 'goals');
     const q = query(goalsRef, where('userId', '==', this.userId));
     return collectionData(q, { idField: 'id' }) as Observable<Goal[]>;
   }
-
   updateGoal(goal: Goal): Observable<void> {
     const goalDocRef = doc(this.firestore, `goals/${goal.id}`);
     return from(setDoc(goalDocRef, goal));
   }
-
   getGoalById(goalId: string): Promise<Goal | null> {
     const goalDocRef = doc(this.firestore, `goals/${goalId}`);
     return getDoc(goalDocRef).then(snapshot => {
@@ -56,8 +47,8 @@ export class GoalService {
       }
     });
   }
-
   deleteGoal(goalId: string) {
     const goalDocRef = doc(this.firestore, `goals/${goalId}`);
+    return deleteDoc(goalDocRef);
   }
 }
