0679e93beb817f5a17c133178d4b0e9b4b4afc4d
MojasSimunovic <mojas0402@gmail.com>
2025-05-25 13:36:11 +0200

    finished all of the functionalities of the app

--- src/app/pages/dashboard/dashboard-chart/dashboard-chart.component.ts
+++ src/app/pages/dashboard/dashboard-chart/dashboard-chart.component.ts
@@ -18,12 +18,12 @@ export class DashboardChartComponent {
     labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     datasets: [
       {
-        data: this.calculateDailySuccess(this.routines()), // This will automatically update
-        label: 'Routine Progress',
+        data: this.calculateDailySuccess(this.routines()),
+        label: 'Weekly Progress',
         fill: true,
         tension: 0.1,
-        borderColor: 'black',
-        backgroundColor: 'rgba(204, 201, 154, 0.3)'
+        borderColor: '#20c997',
+        backgroundColor: 'rgba(39, 43, 46, 0.3)'
       }
     ]
   }));
@@ -32,21 +32,16 @@ export class DashboardChartComponent {
   };
   public lineChartLegend = true;
   calculateDailySuccess(routines: Routine[]): any {
-   const total = routines.length * 7;
-
+    const total = routines.length * 7;
     if (total === 0) return Array(7).fill(0);
-    // Step 1: Count trues per day
     const perDayCounts = routines
       .map(r => r.completions.map(v => v ? 1 : 0))
       .reduce((acc, curr) => acc.map((val, i) => val + curr[i]), Array(7).fill(0));
-    // Step 2: Convert to percentages
     const dailyPercentages = perDayCounts.map(count => Math.round((count / total) * 100));
-    // Step 3: Make cumulative
     const cumulative = dailyPercentages.reduce((acc: number[], val, i) => {
       acc[i] = (acc[i - 1] || 0) + val;
       return acc;
     }, []);
     return cumulative;
   }
-
 }
